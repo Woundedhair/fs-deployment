dca0931e6f8d2142094438da84cf20081f3ee345
Woundedhair <stephentomiwa54@gmail.com>
2024-01-10 19:51:49 -0500

    First commit

--- public/main.js
+++ public/main.js
@@ -1,3 +1,27 @@
 document.querySelector("#button").addEventListener('click', () => {
   document.querySelector("#content").style.color = 'red';
 })
+
+// Button 2
+// Change fn of button2 to a pop-up box for user to type in their e-mail to subscribe to the newsletter 
+    // Use the prompt fn
+
+document.querySelector("#button2").addEventListener('click', () => { 
+    document.querySelector("#newsletter").style.color = 'blue';
+}) // Change fn of button2 to a pop-up box with the prompt fn
+
+// Button 3
+// Change fn of button3 to a pop-up box for user to type in their e-mail to request a one-on-one call 
+    // Use the prompt fn
+
+document.querySelector("#button3").addEventListener('click', () => {
+   // document.querySelector("#lessons").style.color = 'green';
+}) // Change fn of button3 to a pop-up box with the prompt fn
+  prompt("Blue.")
+
+// Button 4
+// Change fn of button4 to a pop-up box for user to type in their e-mail to request a one-on-one call 
+    // Use the prompt fn
+document.querySelector("#button4").addEventListener('click', () => {
+    document.querySelector("#fiber").style.color = 'orange';
+}) // Change fn of button3 to a pop-up box with the prompt fn
